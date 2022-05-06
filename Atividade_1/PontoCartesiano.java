b2e639ff489b77cb817d991af056cf75c7fde87d
CiriloJ <87880569+CiriloJ@users.noreply.github.com>
2022-05-06 17:33:43 -0300

    Create PontoCartesiano.java

---
+++ Atividade_1/PontoCartesiano.java
@@ -0,0 +1,43 @@
+public class PontoCartesiano extends atividade1 {
+	int xa, xb, ya, yb;
+	
+	public PontoCartesiano(int xa, int xb, int ya, int yb){
+		this.xa=xa;
+		this.xb=xb;
+		this.ya=ya;
+		this.yb=yb;
+	}
+
+	public int getXa() {
+		return xa;
+	}
+
+	public void setXa(int xa) {
+		this.xa = xa;
+	}
+
+	public int getXb() {
+		return xb;
+	}
+
+	public void setXb(int xb) {
+		this.xb = xb;
+	}
+
+	public int getYa() {
+		return ya;
+	}
+
+	public void setYa(int ya) {
+		this.ya = ya;
+	}
+
+	public int getYb() {
+		return yb;
+	}
+
+	public void setYb(int yb) {
+		this.yb = yb;
+	}
+	
+}
